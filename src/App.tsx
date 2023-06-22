8ad3d257fd2c21f3de572e2966948edd6eb47433
theunrealhulk <chafiaay.abj@gmail.com>
2023-06-22 12:48:30 +0100

    update component to class components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
-import DropZone from './components/DropZone';
+import DropZone, { DropZoneItem } from './components/DropZone';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 
+interface AppState {
+  dropZone2State: DropZoneItem[];
+}
 
-function App() {
-  return (
-    <div className="App">
-      <Grid container spacing={2}>
-        <Grid item md={6}>
-          <DropZone title="Drop Zone 1"/>          
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      dropZone2State: [],
+    };
+  }
+
+  saveDropZone2Content = () => {
+    console.log(this.state.dropZone2State);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Grid container spacing={2}>
+          <Grid item md={6}>
+            <DropZone title="Drop Zone 1" />
+          </Grid>
+          <Grid item md={6}>
+            <DropZone title="Drop Zone 2" />
+          </Grid>
         </Grid>
-        <Grid item md={6}>
-          <DropZone title="Drop Zone 2"/>
-        </Grid>
-      </Grid>
-      <Grid>
-        <Grid item md={12}>
-            <Button variant="outlined">Save</Button>
-        </Grid>
-        <Grid item md={12}>
+        <Grid container>
+          <Grid item md={12}>
+            <Button variant="outlined" onClick={this.saveDropZone2Content}>
+              Save
+            </Button>
+          </Grid>
+          <Grid item md={12}>
             {/* Drop Zone 2 Data Table */}
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Typography } from '@mui/material';
-import './DropZone.scss';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import { v4 as uuid } from 'uuid';
-
+import './DropZone.scss';
 
 type DropZoneProps = {
   title?: string;
+  onStateChange?: (newState: DropZoneItem[]) => void; // New prop for state change callback
 };
 
 type DropZoneItem = {
@@ -17,51 +17,56 @@ type DropZoneItem = {
   content: React.ReactNode;
 };
 
-const DropZone = ({ title }: DropZoneProps) => {
-  const labelWithSuffix = (label: string) => {
+class DropZone extends Component<DropZoneProps> {
+  state = {
+    data: [
+      {
+        id: uuid(),
+        position: '1',
+        content: <FormControlLabel control={<Checkbox defaultChecked />} label={this.labelWithSuffix('checkbox 1')} />,
+      },
+      {
+        id: uuid(),
+        position: '2',
+        content: <TextField id="filled-basic" label={this.labelWithSuffix('Text Field 1')} variant="filled" />,
+      },
+      {
+        id: uuid(),
+        position: '3',
+        content: <FormControlLabel control={<Checkbox defaultChecked />} label={this.labelWithSuffix('checkbox 2')} />,
+      },
+      {
+        id: uuid(),
+        position: '4',
+        content: <TextField id="filled-basic" label={this.labelWithSuffix('Text Field 2')} variant="filled" />,
+      },
+    ],
+  };
+
+  labelWithSuffix(label: string) {
+    const { title } = this.props;
     if (title) {
       return `${label} - ${title}`;
     }
     return label;
-  };
+  }
 
-  const [data, setData] = useState<DropZoneItem[]>([
-    {
-      id: uuid(),
-      position: '1',
-      content: <FormControlLabel control={<Checkbox defaultChecked />} label={labelWithSuffix('checkbox 1')} />,
-    },
-    {
-      id: uuid(),
-      position: '2',
-      content: <TextField id="filled-basic" label={labelWithSuffix('Text Field 1')} variant="filled" />,
-    },
-    {
-      id: uuid(),
-      position: '3',
-      content: <FormControlLabel control={<Checkbox defaultChecked />} label={labelWithSuffix('checkbox 2')} />,
-    },
-    {
-      id: uuid(),
-      position: '4',
-      content: <TextField id="filled-basic" label={labelWithSuffix('Text Field 2')} variant="filled" />,
-    },
-  ]);
+  render() {
+    const { title } = this.props;
+    const { data } = this.state;
 
-  return (
-    <div className="drop-zone">
+    return (
       <div className="drop-zone">
         {title && <Typography variant="h5">{title}</Typography>}
         <ul>
-        {data.map((item) => (
-            <li key={item.id}>
-            {item.content}
-            </li>
-        ))}
+          {data.map((item) => (
+            <li key={item.id}>{item.content}</li>
+          ))}
         </ul>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default DropZone;
+export type { DropZoneItem };
