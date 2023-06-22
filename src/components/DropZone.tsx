bad92723c34f08b0ab130ea7f6369a89a64b6ccb
theunrealhulk <chafiaay.abj@gmail.com>
2023-06-22 08:30:52 +0100

    Update DropZone Input Items and add beautiful-dnd

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,38 +1,65 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
 import { Typography } from '@mui/material';
-import './DropZone.scss'
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
+import './DropZone.scss';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
+import { v4 as uuid } from 'uuid';
+
+
 type DropZoneProps = {
   title?: string;
+};
 
+type DropZoneItem = {
+  id: string;
+  position: string;
+  content: React.ReactNode;
 };
 
 const DropZone = ({ title }: DropZoneProps) => {
-    const labelWithSuffix = (label: string) => {
-        if (title) {
-          return `${label} - ${title}`;
-        }
-        return label;
-      };
+  const labelWithSuffix = (label: string) => {
+    if (title) {
+      return `${label} - ${title}`;
+    }
+    return label;
+  };
+
+  const [data, setData] = useState<DropZoneItem[]>([
+    {
+      id: uuid(),
+      position: '1',
+      content: <FormControlLabel control={<Checkbox defaultChecked />} label={labelWithSuffix('checkbox 1')} />,
+    },
+    {
+      id: uuid(),
+      position: '2',
+      content: <TextField id="filled-basic" label={labelWithSuffix('Text Field 1')} variant="filled" />,
+    },
+    {
+      id: uuid(),
+      position: '3',
+      content: <FormControlLabel control={<Checkbox defaultChecked />} label={labelWithSuffix('checkbox 2')} />,
+    },
+    {
+      id: uuid(),
+      position: '4',
+      content: <TextField id="filled-basic" label={labelWithSuffix('Text Field 2')} variant="filled" />,
+    },
+  ]);
+
   return (
     <div className="drop-zone">
-      {title && <Typography variant="h5">{title}</Typography>}
-      <Grid>
-              <Grid item md={12}>
-              <FormControlLabel control={<Checkbox defaultChecked />} label={labelWithSuffix('checkbox')} />
-              </Grid>
-              <Grid item md={12}>
-                <Switch aria-label='switch zone 1' />
-              </Grid>
-              <Grid item md={12}>
-                <TextField id="filled-basic" label={labelWithSuffix('Text Field')} variant="filled" />
-              </Grid>
-            </Grid>   
+      <div className="drop-zone">
+        {title && <Typography variant="h5">{title}</Typography>}
+        <ul>
+        {data.map((item) => (
+            <li key={item.id}>
+            {item.content}
+            </li>
+        ))}
+        </ul>
+      </div>
     </div>
   );
 };
